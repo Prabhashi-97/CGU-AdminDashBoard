21248edf7544371c1a12fbd8fecb1d41f59e77aa
SashiniDawpadi <86653321+SashiniDawpadi@users.noreply.github.com>
2022-03-04 19:12:43 +0530

    image-album module

---
+++ src/app/image-album/album-list-view/album-list-view.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+// import { Observable } from 'rxjs';
+
+import { Router, ActivatedRoute } from '@angular/router';
+import { AlbumService } from 'src/app/services/album.service';
+
+@Component({
+  selector: 'app-album-list-view',
+  templateUrl: './album-list-view.component.html',
+  styleUrls: ['./album-list-view.component.css'],
+})
+export class AlbumListViewComponent implements OnInit {
+  listOfAlbums: any;
+  albumId: any;
+  constructor(
+    private albumService: AlbumService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((data) => {
+      this.albumId = data['id'];
+    });
+
+    this.albumService.listOfAlbums().subscribe((data: any) => {
+      if (data !== undefined) {
+        console.log(this.listOfAlbums);
+        this.listOfAlbums = data;
+      }
+    });
+  }
+  addAlbumCall() {
+    this.router.navigate(['create']);
+  }
+}
