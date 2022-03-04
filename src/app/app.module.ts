e8f39546431feb3b8e86c1afd312d674ff8d3bf5
Lakshani-Galwatta <85465588+Lakshani-Galwatta@users.noreply.github.com>
2022-03-05 00:04:48 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CompanySessionsModule } from './company-sessions/company-sessions.module';
 
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -29,8 +30,7 @@ import { CompanySessionsModule } from './company-sessions/company-sessions.modul
     VacanciesModule,
     MatTableModule,
     ProgramsModule,  
-    NgbModule,
-    CompanySessionsModule,
+    CompanySessionsModule, NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
